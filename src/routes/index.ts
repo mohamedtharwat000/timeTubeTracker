644a5777240da3c034fce0f24527e4d65c569cc4
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-21 03:43:40 +0200

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import express, { Router, Request, Response } from 'express';
-// import Middleware from '../utils/middleware';
+import Middleware from '../utils/middleware';
 
 const indexRouter: Router = express.Router();
 
-// indexRouter.all('*', Middleware.checkUser);
+indexRouter.all('*', Middleware.checkUser);
 
 indexRouter.get('/', (_req: Request, res: Response) => {
   res.send('Welcom to TimeTubeTracker');
