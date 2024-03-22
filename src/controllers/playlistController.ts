6abe2243bfad86f3f4866554708b41fb9ce402ce
LORDyyyyy <mlord6271@gmail.com>
2024-03-22 03:25:37 +0200

    mulitple playlists

--- src/controllers/playlistController.ts
+++ src/controllers/playlistController.ts
@@ -6,51 +6,74 @@ import redisClient from '../models/storage/redisdb';
 class PlaylistController {
   /**
    * Get the total length of a YouTube Playlist
-   * And in all the available YouTube videos' speed
-   *
-   * @static
-   * @async
-   * @param {Request} req - express Request
-   * @param {Response} res - express Response
-   * @returns {Promise<Response>} A promise containing the response.
    */
-  static async calculatePlaylist(
+  static async calculateMulitplePlaylists(
     req: Request,
     res: Response,
   ): Promise<Response> {
-    const { playlistURL } = req.body;
+    const playLists = req.body.playlists as [];
+    const playlistsData = [];
     const re = /list=([\w-]+)|^([\w-]+)$/;
 
-    if (!playlistURL) {
-      return res.status(400).json({ error: 'Missing Playlist URL' });
-    }
-    const extractedURL = playlistURL
-      .match(re)[0]
-      .split('list=')
-      .filter((e: string) => e)[0];
-    if (!re.test(playlistURL)) {
-      return res.status(400).json({ error: 'Invalid Playlist URL/ID' });
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const playlist of playLists) {
+      const { playlistURL } = playlist;
+      if (!playlistURL) {
+        return res.status(400).json({ error: 'Missing Playlist URL' });
+      }
+      const extractedURL = (playlistURL as string)
+        .match(re)[0]
+        .split('list=')
+        .filter((e: string) => e)[0];
+      if (!re.test(playlistURL)) {
+        return res.status(400).json({ error: 'Invalid Playlist URL/ID' });
+      }
+
+      const { start, end } = playlist;
+
+      const data = await PlaylistController.calculatePlaylist(
+        extractedURL,
+        start, // Use the start property
+        end, // Use the end property
+      );
+
+      // if (data.error) {
+      //   return res.status(400).json(data);
+      // }
+      playlistsData.push(data);
     }
 
-    return YouTubeHandler.fetchPlaylistVideosIDs(extractedURL)
+    return res.status(200).json(playlistsData);
+  }
+
+  /**
+   * Get the total length of a YouTube Playlist
+   */
+
+  static async calculatePlaylist(playlistId: string, reqStart, reqEnd) {
+    // const cachedPlaylist = await redisClient.get(playlistId);
+
+    // if (cachedPlaylist) {
+    //   return JSON.parse(cachedPlaylist);
+    // }
+    return YouTubeHandler.fetchPlaylistVideosIDs(playlistId)
       .then(async (data) => {
         const dataDuration = await YouTubeHandler.fetchVideosDuration(data);
-
-        const start = req.body.start ?? 1;
-        const end = req.body.end ?? dataDuration.length;
-
+        const start = reqStart ?? 1;
+        const end = reqEnd ?? dataDuration.length;
         if (!start || start <= 0 || start > dataDuration.length) {
-          return res.status(400).json({ error: 'Invalid Start index' });
+          return { error: 'Invalid Start index' };
         }
         if (!end || end <= 0 || end > dataDuration.length) {
-          return res.status(400).json({ error: 'Invalid End index' });
+          return { error: 'Invalid End index' };
         }
         if (start > end) {
-          return res.status(400).json({ error: 'Invalid End and Start index' });
+          return { error: 'Invalid End and Start index' };
         }
 
         const fullDurationInMs = {};
         dataDuration.slice(start - 1, end).forEach((ele) => {
+          console.log(playlistId);
           // eslint-disable-next-line no-restricted-syntax
           for (const speed of [1, 1.25, 1.5, 1.75, 2]) {
             fullDurationInMs[`${speed}x`] ??= 0;
@@ -58,20 +81,23 @@ class PlaylistController {
           }
         });
         const fullData = {
-          totalVideos: dataDuration.length,
+          totalPlaylistVideos: dataDuration.length,
+          totalVideos: end - start + 1,
           averageDuration: msToHMS(fullDurationInMs['1x'] / (end - start + 1)),
           indexes: { start, end },
-          ...Object.fromEntries(
-            Object.entries(fullDurationInMs).map(([speed, duration]) => [
-              `${speed}`,
-              msToHMS(duration),
-            ]),
-          ),
+          durations: {
+            ...Object.fromEntries(
+              Object.entries(fullDurationInMs).map(([speed, duration]) => [
+                `${speed}`,
+                msToHMS(duration),
+              ]),
+            ),
+          },
         };
-        redisClient.set(extractedURL, JSON.stringify(fullData), 3600);
-        return res.status(200).json(fullData);
+        // redisClient.set(`${playlistId}`, JSON.stringify(fullData), 3600);
+        return fullData;
       })
-      .catch(() => res.status(400).json({ error: 'Invalid Playlist ID' }));
+      .catch(() => ({ error: 'Invalid Playlist ID' }));
   }
 }
 
